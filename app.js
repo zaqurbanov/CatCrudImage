1f66ca316d7a5d44160ff373f53ddacc6ce4f163
zaurqurbanov <qurbanovzaur078@gmail.com>
2024-07-09 18:26:30 +0400

    Create Public and Express

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("test");
+const express = require('express');
+const app = express();
+const path = require('path')
+
+
+const PORT = 3789;
+
+
+app.use(express.static('public'))
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+})
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./temp/about.html'))
+})
+
+app.listen(PORT,()=>{
+
+console.log(`server Listening on port ${PORT}`);
+
+})
